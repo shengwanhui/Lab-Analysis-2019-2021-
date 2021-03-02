ca9f8e74955af4933012e344dca0b5887e174c21
Scott W Harden <swharden@gmail.com>
2021-03-01 19:51:49 -0500

    examples of custom modules

---
+++ dev/modules/demo.py
@@ -0,0 +1,7 @@
+# import a python script as a module to use its functions
+import science
+
+
+if __name__ == "__main__":
+    total = science.add(3, 5)
+    print(total)
---
+++ dev/modules/science.py
@@ -0,0 +1,13 @@
+"""
+This module holds functions to perform special science operations.
+This file is not intended to be run directly.
+This file is intended to be imported by other files.
+"""
+
+
+def add(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    print("DO NOT RUN THIS PROGRAM DIRECTLY!")
